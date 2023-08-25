895cb1241e7fbaabd39d5b30ee3406d9c46dcdeb
i7-8700 <1281261856@qq.com>
2023-08-25 10:44:48 +0800

    product list

---
+++ src/components/productCollection/ProductImage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Image, Typography } from "antd";
+
+export interface ProductImageProps {
+  id: string | number;
+  size: "large" | "small";
+  src: string;
+  price: number | string;
+  title: string;
+}
+
+export const ProductImage: React.FC<ProductImageProps> = ({ id, size, src, price, title }) => {
+  return (
+    <>
+      {size == 'large'
+        ? (<Image src={src} height={285} width={490} />)
+        : (<Image src={src} height={120} width={240} />)}
+      
+      <div>
+        <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
+        <Typography.Text type="danger" strong>￥{price}起</Typography.Text>
+      </div>
+    </>
+  );
+};
