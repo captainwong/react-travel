43b201ae3ad29b3581789baea54fd004e2d20cb4
i7-8700 <1281261856@qq.com>
2023-08-26 17:54:34 +0800

    axios

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,23 @@ import App from './App';
 import './i18n/config';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+
+axios.defaults.headers["x-icode"] = 'foo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+/*
+<React.StrictMode> intentionally invokes certain React lifecycle methods twice, 
+including componentDidMount, to help detect potential issues with the code.
+<React.StrictMode> is designed to highlight potential problems,
+such as side effects, and warn about them during development.
+It happens only in development environment,
+while in production componentDidMount is called only once even with <React.StrictMode>.
+*/
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,72 @@
 import React from "react";
-import { Row, Col, Typography } from 'antd';
+import { Row, Col, Typography, Spin } from 'antd';
 import styles from './HomePage.module.css';
 import { Header, Footer, SideMenu, Carousel, ProductCollection, BusinessPartner } from '../../components';
-import { productList1, productList2, productList3 } from './mockups';
 import sideImage1 from '../../assets/images/sider_2019_12-09.png';
 import sideImage2 from '../../assets/images/sider_2019_02-04.png';
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png';
 import { withTranslation, WithTranslation } from "react-i18next";
+import axios from 'axios';
 
-class HomePageComponent extends React.Component<WithTranslation> {
+interface HomeState {
+  loading: boolean;
+  error: string | null;
+  producList: any[];
+}
+
+class HomePageComponent extends React.Component<WithTranslation, HomeState> {
+  constructor(props: WithTranslation) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      producList: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('http://192.168.50.162:3001/api/productCollections');
+      console.log(data);
+      this.setState({
+        loading: false,
+        error: null,
+        producList: data
+      });
+    } catch (e) {
+      console.log(e);
+      if (e instanceof Error) {
+        this.setState({
+          loading: false,
+          error: e.message,
+        });
+      }
+    }
+  }
+
   render() {
     const { t } = this.props;
+    const { producList, loading, error } = this.state;
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+          }}
+        />
+      );
+    }
+
+    if (error) {
+      return <div>Error: {error}</div>
+    }
+
     return (
       <>
         <Header />
@@ -31,7 +87,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
             </Typography.Title>
           }
             sideImage={sideImage1}
-            products={productList1}
+            products={producList[0].touristRoutes}
           />
 
           <ProductCollection title={
@@ -40,7 +96,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
             </Typography.Title>
           }
             sideImage={sideImage2}
-            products={productList2}
+            products={producList[1].touristRoutes}
           />
 
           <ProductCollection title={
@@ -49,7 +105,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
             </Typography.Title>
           }
             sideImage={sideImage3}
-            products={productList3}
+            products={producList[2].touristRoutes}
           />
 
         </div>
