39a479b9de13020b6319a6a136a02fa052695de3
i7-8700 <1281261856@qq.com>
2023-08-26 15:54:50 +0800

    beautiful function component with redux

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,54 +1,79 @@
 import React from "react";
 import { Layout, Typography, Input, Dropdown, Menu, Button } from 'antd';
+import type { MenuProps } from "antd";
 import { GlobalOutlined } from '@ant-design/icons';
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
+import {
+  LanguageActionTypes,
+  addLanguageActionCreator,
+  changeLanguageActionCreator
+} from "../../redux/language/languageActions";
+import { useSelector } from "../../redux/hooks";
+
 import styles from './Header.module.css';
 import logo from '../../assets/logo.svg';
 
 export const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const language = useSelector((state) => state.language);
+  const languageList = useSelector((state) => state.languageList);
+  const dispatch = useDispatch<Dispatch<LanguageActionTypes>>();
+  const { t } = useTranslation();
 
-  const items = [
-    { key: "1", label: "中文" },
-    { key: "2", label: 'English' },
-  ];
+  const langMenuHandler: MenuProps['onClick'] = (e) => {
+    if (e.key === 'new') {
+      dispatch(addLanguageActionCreator('Clingon', 'clingon'));
+    } else {
+      dispatch(changeLanguageActionCreator(e.key === 'zh' ? 'zh' : 'en'));
+    }
+  }
+
+  const langMenuProps = {
+    items: [
+      ...languageList.map(lang => { return { label: lang.name, key: lang.code } }),
+      { label: t("header.add_new_language"), key: 'new' },
+    ],
+    onClick: langMenuHandler
+  };
 
   const mainMenuItems = [
-    { key: "1", label: "旅游首页" },
-    { key: "2", label: "周末游" },
-    { key: "3", label: "跟团游" },
-    { key: "4", label: "自由行" },
-    { key: "5", label: "私家团" },
-    { key: "6", label: "邮轮" },
-    { key: "7", label: "酒店+景点" },
-    { key: "8", label: "当地玩乐" },
-    { key: "9", label: "主题游" },
-    { key: "10", label: "定制游" },
-    { key: "11", label: "游学" },
-    { key: "12", label: "签证" },
-    { key: "13", label: "企业游" },
-    { key: "14", label: "高端游" },
-    { key: "15", label: "爱玩户外" },
-    { key: "16", label: "保险" },
+    { key: "1", label: t("header.home_page") },
+    { key: "2", label: t("header.weekend") },
+    { key: "3", label: t("header.group") },
+    { key: "4", label: t("header.backpack") },
+    { key: "5", label: t("header.private") },
+    { key: "6", label: t("header.cruise") },
+    { key: "7", label: t("header.hotel") },
+    { key: "8", label: t("header.local") },
+    { key: "9", label: t("header.theme") },
+    { key: "10", label: t("header.custom") },
+    { key: "11", label: t("header.study") },
+    { key: "12", label: t("header.visa") },
+    { key: "13", label: t("header.enterprise") },
+    { key: "14", label: t("header.high_end") },
+    { key: "15", label: t("header.outdoor") },
+    { key: "16", label: t("header.insurance") },
   ];
-
-  const navigate = useNavigate();
 
   return (
     <div className={styles['app-header']} >
       {/* top-header */}
       <div className={styles['top-header']}>
         <div className={styles.inner}>
-          <Typography.Text>Make travel happier</Typography.Text>
+          <Typography.Text>{t("header.slogan")}</Typography.Text>
           <Dropdown.Button className={styles['lang-menu']}
             style={{ marginLeft: 15 }}
-            menu={{ items }}
+            menu={langMenuProps}
             icon={<GlobalOutlined />}
           >
-            Language
+            {language === 'zh' ? '中文' : 'English'}
           </Dropdown.Button>
           <Button.Group className={styles['button-group']}>
-            <Button onClick={() => navigate('/signup')}>Sign Up</Button>
-            <Button onClick={() => navigate('/signin')}>Sign In</Button>
+            <Button onClick={() => navigate('/signup')}>{t("header.register")}</Button>
+            <Button onClick={() => navigate('/signin')}>{t("header.signin")}</Button>
           </Button.Group>
         </div>
       </div>
@@ -57,7 +82,7 @@ export const Header: React.FC = () => {
       <Layout.Header className={styles['main-header']}>
         <span onClick={() => navigate('/')} >
           <img src={logo} alt="logo" className={styles['App-logo']} />
-          <Typography.Title level={3} className={styles['title']}>React Travel</Typography.Title>
+          <Typography.Title level={3} className={styles['title']}>{t("header.title")}</Typography.Title>
         </span>
 
         <Input.Search placeholder='Please input destination, theme or keyword' className={styles['search-input']}></Input.Search>
