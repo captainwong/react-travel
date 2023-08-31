cac6294d13118af4c4b2d9a2aadf8c22a9665a19
i7-8700 <1281261856@qq.com>
2023-09-01 00:20:31 +0800

    still working on checkout

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -5,12 +5,14 @@ import { Row, Col, Affix } from "antd";
 import { ProductList, PaymentCard } from "../../components";
 import { useSelector, useAppDispatch } from "../../redux/hooks";
 import { removeShoppingCartItems, checkOut } from "../../redux/shoppingCart/slice";
+import { useNavigate } from "react-router-dom";
 
 export const ShoppingCartPage: React.FC = () => {
   const loading = useSelector(s => s.shoppingCart.loading);
   const jwt = useSelector(s => s.user.token) as string;
   const shoppingCartItems = useSelector(s => s.shoppingCart.items);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   return (
     <MainLayout>
@@ -35,7 +37,11 @@ export const ShoppingCartPage: React.FC = () => {
                   shoppingCartItems.map(item => item.price * (item.discountPersent ? item.discountPersent : 1)).reduce((a, b) => { return a + b; }, 0)
                 }
                 onCheckout={() => {
+                  if (shoppingCartItems.length <= 0) {
+                    return;
+                  }
                   dispatch(checkOut(jwt));
+                  navigate('/placeOrder');
                 }}
                 onShppingCartClear={() => {
                   dispatch(removeShoppingCartItems({token: jwt, ids: shoppingCartItems.map(item=>item.id)}));
